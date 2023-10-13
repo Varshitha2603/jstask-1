ef55e40a348a463006f129912114da97067070ea
DCKAP <varshithaksdckap@gmail.com>
2023-10-13 14:15:29 +0530

    task-1

---
+++ index.js
@@ -0,0 +1,10 @@
+// --------------------Task 1-----------------------------------------------------
+
+
+// function myButton() {
+//     document.body.style.backgroundColor="blue";
+//     document.getElementById("demo").innerHTML=Date();
+//     document.querySelector("button").style.backgroundColor="red";
+// }
+
+
